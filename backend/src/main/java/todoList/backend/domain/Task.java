83f32b8d34815ffafd661fc2ecfbed9a16cb3c59
minsuk <legostarwars@naver.com>
2023-01-18 02:33:25 +0900

    Feat: Added Swagger to test backend api

--- backend/src/main/java/todoList/backend/domain/Task.java
+++ backend/src/main/java/todoList/backend/domain/Task.java
@@ -1,12 +1,13 @@
 package todoList.backend.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -14,7 +15,8 @@ import lombok.ToString;
 @Entity
 public class Task {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String text;
